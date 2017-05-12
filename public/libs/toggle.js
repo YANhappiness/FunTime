dce745358b4996410d6cf2c953e0193ce6953f72
happiness <457029997@qq.com>
2017-05-12 10:42:46 +0800

    完成侧边栏的展开与收缩

--- public/libs/toggle.js
+++ public/libs/toggle.js
@@ -19,7 +19,7 @@ menu.onclick = function () {
 			dd[i].style.transitionTimingFunction = 'ease-out';
 			dd[i].style.transform = 'translate(0)';
 		}
-	} else {
+	}else {
 		for(var i=dd.length - 1; i>=0; i--) {
 			dd[i].style.transitionDuration = (dd.length - i + 1) * 0.05 + 's';
 			dd[i].style.transitionProperty = 'all';
